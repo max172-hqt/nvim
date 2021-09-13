5c7e85553de2c4b6af19faa940bd4560b08516fa
Huy Tran <max@gotitapp.co>
2021-09-13 10:09:38 +0700

    feat: Config autopairs and nvim-cmp

--- lua/modules/editor/plugins.lua
+++ lua/modules/editor/plugins.lua
@@ -3,12 +3,27 @@ local function conf(config_name)
   return require(string.format("modules.editor.%s", config_name))
 end
 
-editor['Raimondi/delimitMate'] = {
+editor['windwp/nvim-autopairs'] = {
+  config = conf("nvim-autopairs"),
+  after = 'nvim-cmp'
+}
+
+editor['hrsh7th/nvim-cmp'] = {
+  config = conf('nvim-cmp'),
+  module = 'cmp',
   event = 'InsertEnter',
-  config = conf("delimimate"),
+  requires = {
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-emoji',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-vsnip',
+    { 'hrsh7th/vim-vsnip', requires = { 'rafamadriz/friendly-snippets' } },
+  },
 }
 
 editor['tpope/vim-surround'] = {}
+editor['christoomey/vim-tmux-navigator'] = {}
 
 return editor
 
